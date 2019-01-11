bdea8f26f6b3dca060d36c64bc95f3ac8b0cd615
thu <mingfei.net@gmail.com>
2019-01-11 14:09:00 +0800

    Update.

---
+++ src/main/java/demo/day02/_5_Opreator.java
@@ -0,0 +1,29 @@
+package demo.day02;
+
+/**
+ * 运算符
+ * <p>
+ * 1. 算术运算符
+ */
+public class _5_Opreator {
+    public static void main(String[] args) {
+//        1.
+        int x = 1;
+        int y = 2;
+
+        int z = x + y;
+        System.out.println(z);
+
+        z = x - y;
+        System.out.println(z);
+
+        z = x * y;
+        System.out.println(z);
+
+        z = x / y;
+        System.out.println(z);
+
+        z = x % y;
+        System.out.println(z);
+    }
+}
