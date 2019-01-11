589fc6ec765c23131473d585c1d0a18822d539e9
thu <mingfei.net@gmail.com>
2019-01-11 17:03:38 +0800

    Update.

---
+++ src/main/java/demo/day02/_10_Scanner.java
@@ -0,0 +1,14 @@
+package demo.day02;
+
+import java.util.Scanner;
+
+public class _10_Scanner {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input score: ");
+        int score = scanner.nextInt();
+
+        System.out.println(score);
+    }
+}
