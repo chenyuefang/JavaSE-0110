63d99cda9d430784bf0f1defcbd185722bc7b4c7
thu <mingfei.net@gmail.com>
2019-01-11 14:31:35 +0800

    Update.

--- src/main/java/demo/day02/_5_Opreator.java
+++ src/main/java/demo/day02/_5_Opreator.java
@@ -4,6 +4,7 @@ package demo.day02;
  * 运算符
  * <p>
  * 1. 算术运算符
+ * 2. 关系运算符
  */
 public class _5_Opreator {
     public static void main(String[] args) {
@@ -25,5 +26,36 @@ public class _5_Opreator {
 
         z = x % y;
         System.out.println(z);
+
+        // ++ 自增
+        System.out.println(x++);
+        System.out.println(x++);
+        System.out.println(++x);
+        System.out.println(x);
+
+        // -- 自减
+
+//        2.
+
+        double a = 1.2;
+        double b = 3.4;
+
+        // 行复制 Ctrl + D
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+        System.out.println(a == b);
+        System.out.println(a != b);
+
+        // Java String pool
+        String s1 = "hello";
+//        String s2 = "Hello";
+        String s2 = new String("hello");
+
+        System.out.println(s1);
+        System.out.println(s2);
+
+        System.out.println(s1 == s2);
     }
 }
