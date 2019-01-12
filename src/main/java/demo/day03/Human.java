7a42b1d5357757f9703fcb0f2e11423dfad8b2ca
thu <mingfei.net@gmail.com>
2019-01-12 09:23:57 +0800

    Update.

---
+++ src/main/java/demo/day03/Human.java
@@ -0,0 +1,15 @@
+package demo.day03;
+
+/**
+ * 类 = 域 + 方法
+ */
+public class Human {
+    // 1. 域：类似于变量的定义
+    String name;
+
+    // 2. 方法
+    // void：返回类型，没有返回任何值
+    void study(){
+
+    }
+}
