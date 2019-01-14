1b81a750dc8c38b2eb6453d6e3bb16cef5d73d0b
thu <mingfei.net@gmail.com>
2019-01-14 11:23:31 +0800

    Update.

--- src/main/java/demo/day04/Human.java
+++ src/main/java/demo/day04/Human.java
@@ -19,12 +19,23 @@ package demo.day04;
  *      private 私有 只有当前类内部可以访问
  *      protected 受保护的 同一个包可以访问，外包中当前类的子类的实例可以访问
  *
+ *
+ * 结论：
+ * 类一般是公有的
+ * 域一般是私有的
+ * 方法一般是公有的
+ *
+ * Human 类重写
+ *
+ * 在 Test 中怎么获取 name 的值？
+ * 在 Test 中怎么修改 name 的值？
+ *
  */
 
 
 public class Human {
 
-    protected String name;
+    private String name;
 
     public void study(){
         name = "";
