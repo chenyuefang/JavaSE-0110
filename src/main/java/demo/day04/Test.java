2acd916620ee2de0d00401d315fa8f94683eda68
thu <mingfei.net@gmail.com>
2019-01-14 09:20:56 +0800

    Update.

--- src/main/java/demo/day04/Test.java
+++ src/main/java/demo/day04/Test.java
@@ -8,5 +8,16 @@ public class Test {
         System.out.println(calculator.add(1.1, 2.2)); // 浮点数精度损失
         System.out.println(calculator.add(1, 1.1)); // Ctrl + B
         System.out.println(calculator.add(1.1, 1));
+
+        Square square = new Square(1.2);
+        System.out.println(square.getArea());
+
+        Shape s = new Shape();
+        System.out.println(s.getArea());
+
+        Shape s1 = new Square(2);
+
+//        Square s2 = new Shape(); // ?
+        System.out.println(s1.getArea()); //
     }
 }
