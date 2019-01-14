d4902689827034e1d7576895c53e10df05b3a80a
thu <mingfei.net@gmail.com>
2019-01-14 16:43:11 +0800

    Update.

--- src/main/java/demo/day04/ArrayTest.java
+++ src/main/java/demo/day04/ArrayTest.java
@@ -2,12 +2,10 @@ package demo.day04;
 
 /**
  * Array 数组: 存放同一类型数据的数据结构
- *
- *
  */
 public class ArrayTest {
     public static void main(String[] args) {
-        int[] numbers = new int[10];
+        int[] numbers = new int[10]; // 静态性
 //        int ints [] = new int[10];
 
         numbers[0] = 1850001;
@@ -37,6 +35,28 @@ public class ArrayTest {
         }
 
         // 定义一个数组，可以存放任意类型数据？
+
+        Object[] objects = {123, "hello", 1.23, true};
+
+        int[][] ints = new int[100][999];
+
+        for (int i = 0; i < ints.length; i++) {
+            for (int j = 0; j < ints[i].length; j++) {
+                ints[i][j] = i + j;
+            }
+        }
+
+        int x = ints.length - 1;
+        System.out.println(x);
+        int y = ints[ints.length - 1].length - 1;
+        System.out.println(y);
+        System.out.println(ints[x][y]);
+
+        int[][] ints1 = {
+                {1, 2, 3, 4},
+                {1, 2},
+                {1, 2, 3}
+        };
 
 
     }
