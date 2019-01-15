bbd99f910c89bd3c3f8a1f7dcd27718a70639156
thu <mingfei.net@gmail.com>
2019-01-15 09:24:30 +0800

    Update.

---
+++ src/main/java/demo/day05/StringTest.java
@@ -0,0 +1,22 @@
+package demo.day05;
+
+/**
+ * 集合
+ *
+ * String
+ *
+ *
+ */
+public class StringTest {
+
+    public static void main(String[] args) {
+        String student = "Tom";
+
+        // 1. 构造方法
+        // 选择 3 个构造方法创建一个 String 实例
+
+        // 2. 成员方法
+        // 调用一个成员方法
+
+    }
+}
