d3d7adcad94360ba6d1bf9fdb67e776dc04de80a
thu <mingfei.net@gmail.com>
2019-01-16 16:20:02 +0800

    Update.

--- sql/db.sql
+++ sql/db.sql
@@ -7,27 +7,45 @@ create database db_test;
 
 drop table if exists db_test.student;
 create table db_test.student (
-  id     int auto_increment primary key, # 自增长
-  sno    varchar(20),
-  name   varchar(20) unique,
-  age    int(2) not null,
-  gender char(10),
-  major  varchar(20),
-  city   varchar(20),
-  dob    date, # date of birth
-  height double(2, 1) # 一共有两个数字，小数部分有 1 个数字 。1.x
+  id           int auto_increment primary key, # 自增长
+  name         varchar(20) unique,
+  age          int(2) not null,
+  gender       char(1),
+  dob          date, # date of birth
+  departmentId int
 );
+
+drop table if exists db_test.department;
+create table db_test.department (
+  id    int auto_increment primary key,
+  title varchar(50),
+  tel   varchar(20)
+);
+
+alter table db_test.student
+  add constraint
+  student_fk_departmentId
+foreign key (departmentId)
+references db_test.department (id);
 
 DESC db_test.student; # desc - describe 描述表结构
 
 select *
 from db_test.student; -- 查询语句
 
-insert into db_test.student
-value (1, '185002', 'Tom', 18, 'M', 'SE', 'Beijing', '2000-1-1', 1.7); -- 插入语句 insert
+select *
+from db_test.department;
 
-insert into db_test.student (sno, name, age)
-value ('1850002', 'Tom', 20);
+insert into db_test.student (name, age, gender, dob, departmentId)
+value ('Tom', 20, 'M', '2000-1-1', 1);
+
+insert into db_test.student (name, age, gender, dob, departmentId)
+value ('Jerry', 19, 'F', '2001-1-1', 2);
+
+insert into db_test.department (title, tel)
+value ('CS', '18600000000');
+insert into db_test.department (title, tel)
+value ('EE', '18600000001');
 
 insert into db_test.student (age)
 value (21);
