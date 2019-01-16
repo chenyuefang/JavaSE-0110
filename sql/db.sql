b75d69f42f11c0c517e6cbabf306b3d90c88c31e
thu <mingfei.net@gmail.com>
2019-01-16 11:42:44 +0800

    Update.

--- sql/db.sql
+++ sql/db.sql
@@ -1,7 +1,9 @@
 show databases;
 
+drop database if exists db_test;
 create database db_test;
 
+drop table if exists db_test.student;
 create table db_test.student (
   sno    varchar(20),
   name   varchar(20),
@@ -17,7 +19,7 @@ select * from db_test.student; -- 查询语句
 
 insert into db_test.student value ('185001', 'TOm', 18, 'M', 'SE', 'Beijing'); -- 插入语句 insert
 
-update db_test.student set age = 19; -- 更新 update
+update db_test.student set age = 19, name = 'Tom'; -- 更新 update
 
 delete from db_test.student; -- 删除语句
 
@@ -25,5 +27,5 @@ drop table db_test.student; -- 删除表
 
 show tables from db_test; -- 查询数据库的表
 
-drop database test; -- 删除数据库
+# drop database db_test; -- 删除数据库
 
