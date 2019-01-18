17b6d017562fed28c11cf4347b2bc7dd9830f0dd
thu <mingfei.net@gmail.com>
2019-01-18 10:40:53 +0800

    Update.

---
+++ src/main/java/demo/day08/HelloSwing.java
@@ -0,0 +1,15 @@
+package demo.day08;
+
+import javax.swing.*;
+
+public class HelloSwing {
+
+    public static void main(String[] args) {
+
+        JFrame jFrame = new JFrame("Hello Swing");
+
+        jFrame.setSize(300, 300);
+
+        jFrame.setVisible(true);
+    }
+}
