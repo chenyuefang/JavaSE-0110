4f214017dd43f06785d30226b6a21502e839a7be
thu <mingfei.net@gmail.com>
2019-01-18 09:19:19 +0800

    Update.

--- sql/exec_1.sql
+++ sql/exec_1.sql
@@ -1,43 +1,12 @@
 # 1. 查找部门 30 中员工的详细信息
-select *
-from scott.emp
-where DEPTNO = 30;
 # 2. 找出从事 clerk 工作的员工的编号、姓名、部门号
-select EMPNO, ENAME, DEPTNO
-from scott.emp
-where JOB = 'clerk';
 # 3. 检索出奖金多于基本工资的员工信息
-select *
-from scott.emp
-where COMM > SAL;
 # 4. 检索出奖金多于基本工资 30% 员工信息
-select *
-from scott.emp
-where COMM > SAL * 0.3;
 # 5. 希望看到 10 部门的经理或者 20 部门的职员 clerk 的信息
-select *
-from scott.emp
-where (DEPTNO = 10 and JOB = 'manager')
-   or (DEPTNO = 20 and JOB = 'clerk');
 # 6. 找出 10 部门的经理、20 部门的职员或者既不是经理也不是职员但是 sal 高于 2000 元的员工信息
-select *
-from scott.emp
-where (DEPTNO = 10 and JOB = 'manager')
-   or (DEPTNO = 20 and JOB = 'clerk')
-   or (JOB <> 'manager' and JOB <> 'clerk' and sal > 2000);
-
-select distinct JOB
-from scott.emp;
 # 7. 找出获得奖金的员工的工作
-select JOB, COMM
-from scott.emp
-where comm > 0;
 # 8. 找出奖金少于 100 或者没有获得奖金的员工的信息
-select *
-from scott.emp
-where COMM < 100 or COMM is null ;
 # 9. 查找员工雇佣日期是当月的最后一天的员工信息
-
 # 10. 检索出雇佣年限超过 35 年的员工信息
 # 11. 找出姓名以 A、B、S 开始的员工信息
 # 12. 找到名字长度为 4 个字符的员工信息
@@ -54,4 +23,3 @@ where COMM < 100 or COMM is null ;
 # 23. 至今为止，员工被雇佣的天数
 # 24. 找出姓名中包含 A 的员工信息
 # 25. 计算出员工被雇佣了多少年、多少月、多少日
-
--- sql/query.sql
+++ sql/query.sql
@@ -31,3 +31,44 @@ select *
 from emp
 limit 3 offset 12;
 # limit 0, 3;
+
+select *
+from scott.emp;
+
+select *
+from scott.emp
+where ENAME like '_ing';
+
+select distinct JOB
+from scott.emp;
+
+select *
+from scott.emp
+# where JOB = 'salesman' or JOB = 'manager' or JOB = 'clerk';
+where JOB in ('salesman', 'manager', 'clerk');
+
+select *
+from scott.emp
+# where SAL >= 2000 and sal <= 3000;
+where SAL between 2000 and 3000; # [x, y]
+
+select e.ENAME as '员工姓名'
+from scott.emp e;
+
+select *
+from scott.emp
+where comm is not null; # 判断 is (not)
+
+update scott.emp
+set comm = null # 赋值 =
+where EMPNO = 7499;
+
+# DTL
+
+start transaction ;
+
+delete from scott.emp;
+
+truncate scott.emp;
+
+rollback ;
