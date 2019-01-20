d8161feeb666d6bed003fcfe937e7b508bf1850b
thu <mingfei.net@gmail.com>
2019-01-20 16:41:01 +0800

    Update.

---
+++ src/main/java/demo/day10/ExceptionDemo.java
@@ -0,0 +1,39 @@
+package demo.day10;
+
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+
+public class ExceptionDemo {
+
+    public static void a() throws NullPointerException { // NPE
+        try {
+            b();
+            System.out.println(6);
+        } catch (FileNotFoundException e) {
+            System.out.println(7);
+        }
+        System.out.println(8);
+        throw new NullPointerException();
+    }
+
+    private static void b() throws FileNotFoundException {
+        RandomAccessFile randomAccessFile = new RandomAccessFile("c:/Hello.java", "rw");
+        System.out.println(0);
+    }
+
+    public static void main(String[] args) {
+        try {
+            System.out.println(1);
+            a();
+            System.out.println(2);
+        } catch (Exception e) {
+            System.out.println(3);
+        } finally {
+            System.out.println(4);
+        }
+        System.out.println(5);
+    }
+}
+
+// 178345
+// 1068345
